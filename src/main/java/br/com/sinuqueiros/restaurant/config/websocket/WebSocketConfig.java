8539702c2372849c47696fbee4ed8fc336837eb2
Giovanni Ramos <giovannilramos55@gmail.com>
2023-08-17 18:28:41 -0300

    feat: web socket test

--- src/main/java/br/com/sinuqueiros/restaurant/config/websocket/WebSocketConfig.java
+++ src/main/java/br/com/sinuqueiros/restaurant/config/websocket/WebSocketConfig.java
@@ -1,24 +1,17 @@
 package br.com.sinuqueiros.restaurant.config.websocket;
 
+import br.com.sinuqueiros.restaurant.config.websocket.handler.WebSocketOrderHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
-    public void configureMessageBroker(final MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/list");
-    }
-
-    @Override
-    public void registerStompEndpoints(final StompEndpointRegistry registry) {
-        registry.addEndpoint("/web-socket")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(new WebSocketOrderHandler(), "/chat");
     }
 }
