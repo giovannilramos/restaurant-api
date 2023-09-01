e179262ebbb3afe31f03202ac6e12a828c406500
Giovanni Ramos <giovannilramos55@gmail.com>
2023-08-31 22:24:05 -0300

    feat: cors config

--- src/main/java/br/com/sinuqueiros/restaurant/config/cors/CorsConfig.java
+++ src/main/java/br/com/sinuqueiros/restaurant/config/cors/CorsConfig.java
@@ -2,9 +2,11 @@ package br.com.sinuqueiros.restaurant.config.cors;
 
 import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
